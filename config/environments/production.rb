f447cdc26eebfa1e1661b535d702c7a6c55431c3
axp172130 <anindita.palit98@gmail.com>
2020-04-13 15:46:42 -0500

    heroku errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'flixter-anindita-palit.herokuapp.com'}
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb
   # Code is not reloaded between requests.
   config.cache_classes = true
 
