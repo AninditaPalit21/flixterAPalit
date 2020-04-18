aeab33e99cc5805b4f5cf0df215cab2eb9eaffbd
axp172130 <anindita.palit98@gmail.com>
2020-04-17 23:55:14 -0500

    create course probs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
+
